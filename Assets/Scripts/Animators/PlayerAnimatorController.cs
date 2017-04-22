f491963a53aab4a58a359d3e3666095075fafbd8
BleuDuCiel <quentin.louis@sfr.fr>
2017-04-22 19:13:26 +0200

    fix sprites center

--- Assets/Scripts/Animators/PlayerAnimatorController.cs
+++ Assets/Scripts/Animators/PlayerAnimatorController.cs
@@ -10,24 +10,15 @@ public class PlayerAnimatorController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
-        state = GetComponent<Player>().getState();
-=======
         state = transform.GetComponent<PlayerBehaviour>().GetState();
->>>>>>> ff46afe25e1cfc6f8185ce6f47ba2e08a6bbbe1d
         animator = GetComponent<Animator>();
         audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
-        state = GetComponent<Player>().getState();
-		if(state == Player.State.Walking)
-=======
         state = transform.GetComponent<PlayerBehaviour>().GetState();
 		if(state == PlayerBehaviour.State.Walking)
->>>>>>> ff46afe25e1cfc6f8185ce6f47ba2e08a6bbbe1d
         {
             animator.SetBool("isWalking", true);
             if(!audio.isPlaying)

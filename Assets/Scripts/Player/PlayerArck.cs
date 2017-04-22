79f57fe6b1f202e7413918c70e13a67e9e27d01a
VGanon <yannickjezequel@hotmail.fr>
2017-04-22 14:42:26 +0200

    Add PlayerArck prefab and cie

---
+++ Assets/Scripts/Player/PlayerArck.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player1
+{
+    public class PlayerArck : MonoBehaviour
+    {
+
+        [SerializeField]
+        float horSpeed, verSpeed;
+        Vector3 Up, Down, Right, Left;
+
+        private enum State
+        {
+            Idle,
+            Walking,
+            Hidding,
+            Talking
+        }
+
+        private State state;
+
+        // Use this for initialization
+        void Start()
+        {
+            horSpeed = 1;
+            verSpeed = 1;
+            state = State.Idle;
+            Up = transform.up;
+            Down = -transform.up;
+            Right = transform.right;
+            Left = -transform.right;
+        }
+
+        void Movement()
+        {
+            if (state == State.Idle || state == State.Walking)
+            {
+                Vector3 move = Vector3.zero;
+                float horizontal = Input.GetAxis("Horizontal");
+                float vertical = Input.GetAxis("Vertical");
+                if (vertical > 0)
+                {
+                    move += Up * horSpeed;
+                }
+                else if (vertical < 0)
+                {
+                    move += Down * horSpeed;
+                }
+                if (horizontal > 0)
+                {
+                    move += Right * verSpeed;
+                }
+                else if (horizontal < 0)
+                {
+                    move += Left * verSpeed;
+                }
+                if (move != Vector3.zero)
+                {
+                    state = State.Walking;
+                    transform.position += (move * Time.deltaTime);
+                }
+                else
+                {
+                    state = State.Idle;
+                }
+            }
+        }
+        void Action()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Action();
+            Movement();
+        }
+    }
+}

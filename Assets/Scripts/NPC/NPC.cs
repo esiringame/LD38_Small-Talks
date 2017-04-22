036d9486ea44d23938ee7c8d2bb405c61a5cb5f9
Karamaz0V1 <florent.guiotte@gmail.com>
2017-04-22 13:43:43 +0200

    «I.A.»

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour {
 
-    public float WalkingSpeed = 1.0f;
+    public float WalkingSpeed = 0.8f;
     public Vector3 Direction = Vector3.left;
+
+    public GameObject Player;
+    public float TriggerDistance = 3.0f;
+
+    public bool Triggered = false;
+
     private Transform _transform;
+    private Transform _playerTransform;
 
 	// Use this for initialization
 	void Start () {
 	    _transform = GetComponent<Transform>();
+        _playerTransform = Player.GetComponent<Transform>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		_transform.localPosition += Direction * WalkingSpeed * Time.deltaTime;
+        // Triggered ?
+        Vector2 heading = _transform.position - _playerTransform.position;
+        float playerDistance = heading.magnitude;
+        Triggered = playerDistance < TriggerDistance;
+        Debug.Log("Distance: " + playerDistance);
+
+        // Walk
+        Vector3 oneDirection = Direction;
+        if (Triggered) 
+            oneDirection = - heading / playerDistance;
+        
+		_transform.localPosition += oneDirection * WalkingSpeed * Time.deltaTime;
+
 	}
 }

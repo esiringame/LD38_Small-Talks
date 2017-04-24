40d0ce39371cab5814d80793d3bceec7f8498434
Maxime Broy <broy.maxime@gmail.com>
2017-04-24 20:48:43 +0200

    premiere tentative d'equilibrage

---
+++ Assets/Scripts/Sounds/BinSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinSound : MonoBehaviour {
+
+    GameObject Player;
+    AudioSource music;
+    bool playerLastState;
+	// Use this for initialization
+	void Start () {
+        music = GetComponent<AudioSource>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.GetComponent<PlayerBehaviour>().GetState() == PlayerBehaviour.State.Hidding && playerLastState == false)
+        {
+            music.Play();
+            playerLastState = true;
+        }
+        if (Player.GetComponent<PlayerBehaviour>().GetState() != PlayerBehaviour.State.Hidding && playerLastState == true)
+        {
+            music.Play();
+            playerLastState = false;
+        }
+
+
+    }
+}
